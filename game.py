abb26f1d4a0fed395c4fd80dd9bd320038140025
Jehcky <jehck.tnj@gmail.com>
2023-01-21 11:54:35 -0300

    Some modifications

--- game.py
+++ game.py
@@ -1,35 +1,41 @@
 import pygame
 import random
-import time
+#import time
+import sys
+import os
 
 screen = pygame.display.set_mode((800,600))
 
 def main():
     pygame.init()
-    
+    window_size = window_width, window_height = 800, 600
     pygame.display.set_caption("Memorise")
-    w_surface = pygame.display.get_surface() 
-    game = Game(w_surface)
-    game.play()
-    pygame.quit()
+    intro_img = pygame.image.load(os.path.join('img','intro.png'))
+    screen.blit(intro_img,(80,60))
+    pygame.display.flip()
 
-class Game:
+    # Infinite loop 'til user choose to quit the game
+    while (True):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+class Memorise:
     def __init__(self, surface):
-        pass
-    def update(self):
-        pass
-    def play(self):
-        pass
+        print("This is here so I don't get an error")
 
-class Tile:
+if __name__ == "__main__":
+    main()
+
+'''class Tile:
 
     def __init__(self, pos_x, pos_y, tile_x, tile_y, imagelist, surface):
-      self.pos_x = pos_x
-      self.pos_y = pos_y
-      self.numx = tile_x
-      self.numy = tile_y
-      self.imagelist = imagelist
-      self.surface = surface
+        self.pos_x = pos_x
+        self.pos_y = pos_y
+        self.numx = tile_x
+        self.numy = tile_y
+        self.imagelist = imagelist
+        self.surface = surface
 
     @classmethod
     def set_surface(cls, surface):
@@ -38,7 +44,5 @@ class Tile:
     def draw(self):
         x = 0
         y = 0
-        screen.blit(pygame.image.load(self.imagelist),(self.pos_x,self.pos_y))
+        screen.blit(pygame.image.load(self.imagelist),(self.pos_x,self.pos_y))'''
 
-if __name__ == "__main__":
-    main()
